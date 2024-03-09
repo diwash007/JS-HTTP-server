eae658f3ce1fbd4c8fe18b4dae58cc8265807c22
diwash007 <diwashdahal75@gmail.com>
2024-03-09 23:08:09 +0545

    respond with content

--- app/main.js
+++ app/main.js
@@ -1,28 +1,28 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
     server.close();
   });
 
-  // socket.on("data", (data) => {
-  //   socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  //   socket.close();
-  // });
-
   socket.on("data", (data) => {
     const [method, path, version] = data.toString().split(" ");
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (path.slice(0, 6) === "/echo/") {
+      const text = path.slice(6);
+      socket.write(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
+          text.length +
+          "\r\n\r\n" +
+          text +
+          "\r\n\r\n"
+      );
     } else {
       socket.write("HTTP/1.1 404\r\n\r\n");
     }
-    socket.close();
+    socket.end();
   });
 });
 
