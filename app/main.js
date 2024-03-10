0d901aa85c462036be2e1f1cb348495b32c1770a
diwash007 <diwashdahal75@gmail.com>
2024-03-10 12:37:05 +0545

    refactor server

--- app/main.js
+++ app/main.js
@@ -1,12 +1,9 @@
 const fs = require("fs");
 const net = require("net");
 const path = require("path");
+const directory = resolveDirectory();
 
-let directory = "./";
-let directoryArgIndex = process.argv.findIndex((arg) => arg === "--directory");
-if (directoryArgIndex >= 0) {
-  directory = process.argv[directoryArgIndex + 1];
-}
+const allowedPaths = ["/echo/", "/user-agent", "/files/"];
 
 const server = net.createServer((socket) => {
   socket.on("close", () => {
@@ -15,32 +12,63 @@ const server = net.createServer((socket) => {
   });
 
   socket.on("data", (data) => {
-    const [method, reqPath, version] = data.toString().split(" ");
+    const reqPath = getPath(data);
+
     if (reqPath === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (reqPath.slice(0, 6) === "/echo/") {
+      socket.write(makeStatus("200 Ok"));
+      return;
+    }
+
+    if (!allowedPaths.some((p) => reqPath.startsWith(p))) {
+      console.log("hiyaa");
+      socket.write(makeStatus("404 NOT_FOUND"));
+      return;
+    }
+
+    bind("/echo/", data, (data) => {
       const text = reqPath.slice(6);
+      console.log(text);
+      console.log(reqPath);
       socket.write(makeResponse(text));
-    } else if (reqPath === "/user-agent") {
+    });
+
+    bind("/user-agent", data, (data) => {
       const lines = data.toString().split("\r\n");
       const userAgent = lines[2].split(" ")[1];
       socket.write(makeResponse(userAgent));
-    } else if (reqPath.startsWith("/files/")) {
+    });
+
+    bind("/files/", data, (data) => {
       const fileName = reqPath.slice("/files/".length);
-      const filePath = path.resolve(process.argv[3], fileName);
+      const filePath = path.resolve(directory, fileName);
       if (!fs.existsSync(filePath)) {
         socket.write("HTTP/1.1 404 NOT_FOUND\r\n\r\n");
-        socket.end();
+        return socket.end();
       }
       const fileContent = fs.readFileSync(filePath);
       socket.write(makeResponse(fileContent, "application/octet-stream"));
-    } else {
-      socket.write("HTTP/1.1 404\r\n\r\n");
-    }
+    });
   });
 });
 
 server.listen(4221, "localhost");
+
+function bind(path, data, resolver) {
+  const reqPath = getPath(data);
+  if (reqPath.startsWith(path)) {
+    return resolver(data);
+  }
+}
+
+// UTILS
+function makeStatus(status) {
+  return "HTTP/1.1 " + status + "\r\n\r\n";
+}
+
+function getPath(data) {
+  const [method, reqPath, version] = data.toString().split(" ");
+  return reqPath;
+}
 
 function makeResponse(value, contentType = "text/plain") {
   return (
@@ -51,4 +79,15 @@ function makeResponse(value, contentType = "text/plain") {
     "\r\n\r\n" +
     value
   );
+}
+
+function resolveDirectory() {
+  let directory = "./";
+  let directoryArgIndex = process.argv.findIndex(
+    (arg) => arg === "--directory"
+  );
+  if (directoryArgIndex >= 0) {
+    directory = process.argv[directoryArgIndex + 1];
+  }
+  return directory;
 }
