9386064f774c3c8a297f4084cdbb130ff171e074
diwash007 <diwashdahal75@gmail.com>
2024-03-10 11:59:30 +0545

    Get a file

--- app/main.js
+++ app/main.js
@@ -1,4 +1,12 @@
+const fs = require("fs");
 const net = require("net");
+const path = require("path");
+
+let directory = "./";
+let directoryArgIndex = process.argv.findIndex((arg) => arg === "--directory");
+if (directoryArgIndex >= 0) {
+  directory = process.argv[directoryArgIndex + 1];
+}
 
 const server = net.createServer((socket) => {
   socket.on("close", () => {
@@ -7,27 +15,21 @@ const server = net.createServer((socket) => {
   });
 
   socket.on("data", (data) => {
-    const [method, path, version] = data.toString().split(" ");
-    if (path === "/") {
+    const [method, reqPath, version] = data.toString().split(" ");
+    if (reqPath === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (path.slice(0, 6) === "/echo/") {
-      const text = path.slice(6);
-      socket.write(
-        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
-          text.length +
-          "\r\n\r\n" +
-          text +
-          "\r\n\r\n"
-      );
-    } else if (path === "/user-agent") {
+    } else if (reqPath.slice(0, 6) === "/echo/") {
+      const text = reqPath.slice(6);
+      socket.write(makeResponse(text));
+    } else if (reqPath === "/user-agent") {
       const lines = data.toString().split("\r\n");
       const userAgent = lines[2].split(" ")[1];
-      socket.write(
-        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
-          userAgent.length +
-          "\r\n\r\n" +
-          userAgent
-      );
+      socket.write(makeResponse(userAgent));
+    } else if (reqPath.startsWith("/files/")) {
+      const fileName = reqPath.slice("/files/".length);
+      const filePath = path.resolve(process.argv[3], fileName);
+      const fileContent = fs.readFileSync(filePath);
+      socket.write(makeResponse(fileContent, "application/octet-stream"));
     } else {
       socket.write("HTTP/1.1 404\r\n\r\n");
     }
@@ -35,3 +37,14 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(4221, "localhost");
+
+function makeResponse(value, contentType = "text/plain") {
+  return (
+    "HTTP/1.1 200 OK\r\nContent-Type: " +
+    contentType +
+    "\r\nContent-Length: " +
+    value.length +
+    "\r\n\r\n" +
+    value
+  );
+}
